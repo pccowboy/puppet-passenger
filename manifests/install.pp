715b24df2ebcb852abeed81595be9be81dfd584c
David Swift <dswift@sugarinc.com>
2012-03-02 12:27:51 -0800

    Add amazon OS type

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,6 +1,6 @@
 class passenger::install {
   case $::operatingsystem {
-    redhat,centos,fedora,Scientific: {
+    redhat,centos,fedora,Scientific,Amazon: {
       include passenger::install::redhat
     }
     Debian,Ubuntu: {
